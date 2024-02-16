928c77953eb8decf7711db5487fcf3d474a4dc72
MilanLucas <MilanLucas2001@outlook.com>
2024-02-16 09:20:43 +0100

    functional database creation #1

--- db_Repository.php
+++ db_Repository.php
@@ -2,28 +2,51 @@
 <?php
 $servername = "localhost";
 $username = "milan_lucas_web_shop";
-$password = ".yG(CQgVpk-W85GK";
+$password = "kxFap1pG3rP3rGCT";
+$dbName = 'milan_web_shop_users';
+$conn = dbConnect($servername, $username, $password, $dbName);
 
+dbConnect($conn);
+//dbCreateDatabase($conn);
+//dbCreateTable($conn);
+  
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+function dbConnect($servername, $username, $password, $dbName){
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $dbName);
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  return $conn;
 }
 
-// Create database
-$sql = "CREATE DATABASE myDB";
-if (mysqli_query($conn, $sql)) {
-  echo "Database created successfully";
-} else {
-  echo "Error creating database: " . mysqli_error($conn);
+function dbCreateDatabase($conn){
+  // Create database
+  $sql = "CREATE DATABASE milan_web_shop_users";
+  if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+  } else {
+    echo "Error creating database: " . mysqli_error($conn);
+  }
+  mysqli_close($conn);
 }
+
+function dbCreateTable($conn){
+  $sql = "CREATE TABLE milan_webshop (
+  email VARCHAR(50) NOT NULL PRIMARY KEY,
+  user VARCHAR(30) NOT NULL,
+  password VARCHAR(30) NOT NULL
+  )";
+  if (mysqli_query($conn, $sql)) {
+    echo "Table milan_webshop created successfully";
+  } else {
+    echo "Error creating table: " . mysqli_error($conn);
+  }
+}
+
 
 mysqli_close($conn);
-
-
-
 ?>
 
 
