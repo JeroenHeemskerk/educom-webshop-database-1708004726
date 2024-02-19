47535d361ee3a3f0d436b2f47666f0b0670882d2
MilanLucas <MilanLucas2001@outlook.com>
2024-02-19 09:10:58 +0100

    SQL database #2

--- database/milan_web_shop_users.sql
+++ database/milan_web_shop_users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 16 feb 2024 om 11:29
+-- Gegenereerd op: 19 feb 2024 om 09:10
 -- Serverversie: 10.4.32-MariaDB
 -- PHP-versie: 8.2.12
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `milan_webshop` (
   `email` varchar(50) NOT NULL,
   `user` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `password` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -38,10 +38,18 @@ CREATE TABLE `milan_webshop` (
 --
 
 INSERT INTO `milan_webshop` (`email`, `user`, `password`) VALUES
+('again@email.nl', 'Again', '$2y$14$wZaqqM6mGuYiGabHjNLW4Oj'),
+('e@mail.test', 'test', '$2y$14$8/QPCkQ6TOFXHA7SEEhmcuk/S25JtYIYJyE/YUtErjl1tG9SZkrem'),
+('email@emailer.nl', 'test', '$2y$14$EDBxtFhwHpwLJVf6iJN7Le6MfeG5ZhXxiwgTr9iUCJK20MeGL9bYa'),
 ('email@test.nl', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'test'),
+('t@est.nl', 'test', '$2y$14$1Mhns8K1EcqvAJZ.pAelDufw2z9eNovqhniOTlcZuX6GUqdVeReCO'),
 ('test2@electricboogaloo.nl', '123456789012345678901234567890', 'test'),
+('test@email.nl', 'test', '$2y$14$jBZwlppXEZcCgevgBntcJuqlEMXBfhnoWddpKh5kZTx2wddWAUGIG'),
 ('test@test.nl', 'Name', 'test'),
-('test@tester.nl', 'Dickens', 'PutMeInCoach');
+('test@tester.nl', 'Dickens', 'PutMeInCoach'),
+('tester@tester.nl', 'tester', '$2y$14$9sxutKwo7p4hM5CDVu1iruUMMuGlyvdLRx9e4mwW5t4/fQSoUmdo.'),
+('testtest@email.com', 'test', '$2y$14$Sr77D6TpUYe0WRDbVBO0QOP'),
+('twist@twister.nl', 'test', '$2y$14$Dor5TIjexAKdKuOFhvbi4e8KrRtpyeBwdgkR3z.jP0AuOLw8vAmi6');
 
 --
 -- Indexen voor geëxporteerde tabellen
